704365119362914618874d38aa5fd57524e06fb6
Andrew Domingues <drewdomi@proton.me>
2023-08-21 15:59:28 -0300

    feat: UsersTable Component

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,13 @@
 import SearchBox from "../components/SeachBox";
 import Title from "../components/Title";
+import UsersTable from "../components/UsersTable";
 
 function Home() {
   return (
     <>
       <Title>List Users</Title>
       <SearchBox/>
+      <UsersTable/>
     </>
   );
 }
