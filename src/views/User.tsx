93c053c90aa106429abd766dd88e6cb5c7b5daeb
Andrew Domingues <drewdomi@proton.me>
2023-08-22 17:51:39 -0300

    feat: initial UserCard implementation

--- src/views/User.tsx
+++ src/views/User.tsx
@@ -1,10 +1,16 @@
 import ReturnHomeButton from "../components/ReturnHomeButton";
+import UserCard from "../components/UserCard";
 
 function User() {
   return (
     <>
       <ReturnHomeButton/>
-      <h1>User</h1>
+      <UserCard
+        image="https://randomuser.me/api/portraits/women/19.jpg"
+        altImage="Image of Lisiane Carvalho"
+        name="Lisiane Carvalho"
+        title="Ms"
+      />
     </>
   );
 }
