b27ace8bd8d15f79d5f98a18cc0801359b99e93c
Andrew Domingues <drewdomi@proton.me>
2023-08-25 15:08:50 -0300

    refactor: add react query to User page

--- src/views/User.tsx
+++ src/views/User.tsx
@@ -1,70 +1,57 @@
 import ReturnHomeButton from "../components/ReturnHomeButton";
 import UserCard from "../components/UserCard";
 import UserTabs from "../components/UserTabs";
-import { useEffect, useState } from "react";
 import api from "../services/api";
 import { IGetResponse, IUsersTable } from "../services/models/IUserData";
-import Title from "../components/Title";
 import Loader from "../components/Loader";
+import { useQuery } from "@tanstack/react-query";
+import Title from "../components/Title";
 
 function User() {
   const uuid = window.location.pathname.slice(6);
-  const [user, setUser] = useState<IUsersTable[]>([]);
-  const [loader, setLoader] = useState(true);
 
   function searchByUuid(uuid: string, users: IUsersTable[]) {
-    return users.filter(
+    return users.find(
       (user: IUsersTable) => user.login.uuid.includes(uuid)
     );
   };
 
-  async function getUser() {
-    try {
+  const userQuery = useQuery({
+    queryKey: ["user"],
+    queryFn: () => {
       if (!localStorage.getItem("users")) {
-        const response = await api.get<IGetResponse>("");
-        localStorage.setItem("users", JSON.stringify(response.data.results));
-        setUser(searchByUuid(uuid, response.data.results));
-      }
-      else {
+        api.get<IGetResponse>("").then(res => {
+          localStorage.setItem("users", JSON.stringify(res.data.results));
+          return searchByUuid(uuid, res.data.results);
+        });
+      } else {
         const userFound = JSON.parse(localStorage.getItem("users") || "{}");
-        setUser(searchByUuid(uuid, userFound));
+        return searchByUuid(uuid, userFound);
       }
-    } catch (error) {
-      console.error("Error fetching data:", error);
     }
-  }
+  });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      await getUser();
-    };
-    fetchData().finally(() => {
-      setLoader(false);
-    });
-  }, []);
+  if (userQuery.isLoading) return <Loader />;
+  if (!userQuery.data) {
+    setTimeout(() => window.location.href = "/", 3000)
+    return (
+      <>
+        <ReturnHomeButton />
+        <Title>User not Found</Title>
+        <p>Returning to Home</p>
+      </>)
+  }
 
   return (
     <>
-    {loader && (
-        <Loader/>
-      )}
       <ReturnHomeButton />
-      {!loader && user.length > 0 && (
-        <>
-          <UserCard
-            image={user[0].picture.large}
-            altImage={`Photo of ${user[0].name.first} ${user[0].name.last}`}
-            name={`${user[0].name.first} ${user[0].name.last}`}
-            title={`${user[0].name.title}`}
-          />
-          <UserTabs userInfo={user[0]} />
-        </>
-      )}
-      {!loader && user.length === 0 && (
-        <>
-          <Title>User not found</Title>
-        </>
-      )}
+      <UserCard
+        image={userQuery.data.picture.large}
+        altImage={`Photo of ${userQuery.data.name.title} ${userQuery.data.name.last}`}
+        name={`${userQuery.data.name.first} ${userQuery.data.name.last}`}
+        title={`${userQuery.data.name.title}`}
+      />
+      <UserTabs userInfo={userQuery.data} />
     </>
   );
 }
